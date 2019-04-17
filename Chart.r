b68dfa8313d24cc12414e1169a4a039408a78878
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-04-17 00:35:15 -0400

    Create Chart.r

---
+++ Chart.r
@@ -0,0 +1,17 @@
+data htwt;
+input subject gender $ height weight;
+datalines;
+1 M 68.5 155
+2 F 61.2 99
+3 F 63.0 115
+4 M 70.0 205
+5 M 68.6 170
+6 F 65.1 125
+7 M 72.4 220
+8 M .    188
+;
+
+proc chart data= htwt;
+title "Bar chart from Proc Chart";
+Vbar gender;
+run;
